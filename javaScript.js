1e4199a85686eecfd8508727f50725167a0231c7
MasterX <abellferriol@gmail.com>
2024-07-27 12:02:38 -0400

    nuevas funciones: -definir el nombre por el usuario -saludar al usuario

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,18 @@
 const input = document.querySelector('.numberInput');
 const para = document.querySelector('p');
 
-function showMessage(from, text = "No text given") {
+function showMessage(from = "User", text = "No text given") {
     input.value = '';
     from = '*' + from + '*';
     return from + ': ' + text + "<br>";
 }
 
-let from = "Ann";
+function saludar(name) {
+    return name || "User";
+}
+
+let from = prompt("Cual es tu nombre?");
+alert(saludar(from));
 
 input.addEventListener("change", () => {
     document.getElementById("message").innerHTML += showMessage(from, input.value);
